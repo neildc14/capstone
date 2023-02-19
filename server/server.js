656110d60c0682111e9755533a80924b15817545
Neil Edward Dela Cruz <63070686+neildc14@users.noreply.github.com>
2023-02-19 11:40:17 +0800

    socket.io

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const http = require("http");
+const { Server } = require("socket.io");
 const requests = require("./routes/requests");
 const ambulance = require("./routes/ambulance");
 const ticket = require("./routes/ticket");
@@ -26,7 +27,28 @@ app.use("/api/request", requests);
 app.use("/api/ambulance", ambulance);
 app.use("/api/ticket", ticket);
 app.use("/api/auth", users);
-app.use("/api/schedule", schedule); 
+app.use("/api/schedule", schedule);
+
+//websocket
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`USER CONNECTED ${socket.id}`);
+
+  socket.on("join_map", (data) => {   
+    socket.join(data);
+  });
+
+  socket.on("send_location", (data) => {
+    console.log(data.toString);
+    socket.to(data.map).emit("receive_location", data);
+  });
+});
 
 mongoose.set("strictQuery", true);
 mongoose.connect(process.env.MONGODB_URI, {
@@ -41,4 +63,4 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 // launch our backend into a port
 server.listen(process.env.API_PORT, () =>
   console.log(`Listening on port ${process.env.API_PORT}`)
-);
+);  
