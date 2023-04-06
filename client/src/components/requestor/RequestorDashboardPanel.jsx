f92bccd8be50bf5cdc56b6a605c0414168857550
Neil Edward Dela Cruz <63070686+neildc14@users.noreply.github.com>
2023-04-06 10:23:12 +0800

    feature:button request on requestDashboard

--- client/src/components/requestor/RequestorDashboardPanel.jsx
+++ client/src/components/requestor/RequestorDashboardPanel.jsx
@@ -9,6 +9,8 @@ import {
   CardBody,
   Grid,
   GridItem,
+  Button,
+  useMediaQuery,
 } from "@chakra-ui/react";
 import {
   UilThLarge,
@@ -17,16 +19,17 @@ import {
 } from "@iconscout/react-unicons";
 import RequestCard from "./RequestorRequestCard";
 import TripTicket from "../TripTicket";
-import PanelCard from "../PanelCard";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import PersonnelPanelCard from "../global/PanelCard";
+import { useNavigate } from "react-router-dom";
 
 const ENDPOINT = import.meta.env.VITE_REACT_APP_ENDPOINT;
 
 const RequestorDashboardPanel = () => {
   const [totalRequest, setTotalRequest] = useState("");
   const [totalSuccessfulTransport, setTotalSuccessfulTransport] = useState("");
+  const [isLargerThan768] = useMediaQuery("(min-width: 768px)");
 
   const fetchRecentRequestAndTicket = async () => {
     const results = await Promise.allSettled([
@@ -44,7 +47,6 @@ const RequestorDashboardPanel = () => {
       refetchOnWindowFocus: false,
     }
   );
-
   useEffect(() => {
     if (!isLoading && !isFetching) {
       setTotalRequest(data[0]?.value?.data.length || 0);
@@ -59,9 +61,14 @@ const RequestorDashboardPanel = () => {
       total: 0,
       type: "Approved",
     },
-    { title: "Total Requests", total: totalRequest, type: "Fulfilled" },
+    { title: "Total Requests", total: totalRequest || 0, type: "Fulfilled" },
     { title: "Total Requests", total: 0, type: "Rejected" },
   ];
+
+  const navigate = useNavigate();
+  const handleRequestClick = () => {
+    navigate("request");
+  };
 
   return (
     <>
@@ -73,7 +80,7 @@ const RequestorDashboardPanel = () => {
         >
           <Box as="section">
             <Box>
-              <Flex justifyContent="space-between" alignItems="center">
+              <Flex justifyContent="space-between" alignItems="center" pb={2}>
                 <Heading
                   as="h2"
                   display="inline-flex"
@@ -86,6 +93,19 @@ const RequestorDashboardPanel = () => {
                 >
                   <UilThLarge color="#FF7A00" /> Dashboard Panel
                 </Heading>
+                {isLargerThan768 && (
+                  <Button
+                    size="md"
+                    px={10}
+                    bgColor="red.600"
+                    color="white"
+                    _hover={{ bgColor: "red.700" }}
+                    fontSize="lg"
+                    onClick={handleRequestClick}
+                  >
+                    Request
+                  </Button>
+                )}
               </Flex>
               <Divider />
             </Box>
@@ -112,6 +132,22 @@ const RequestorDashboardPanel = () => {
           </Box>
 
           <Box as="section" pb={4}>
+            {!isLargerThan768 && (
+              <Box mb={4}>
+                <Button
+                  size="md"
+                  width="100%"
+                  px={10}
+                  bgColor="red.600"
+                  color="white"
+                  _hover={{ bgColor: "red.700" }}
+                  fontSize="lg"
+                  onClick={handleRequestClick}
+                >
+                  Request
+                </Button>
+              </Box>
+            )}
             <Box mb={8} border="1px solid #D9D9D9">
               <Heading
                 as="h2"
