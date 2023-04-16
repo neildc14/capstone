021e7aff14cf9d8a28164b9e978e8305ff5388d9
Neil Edward Dela Cruz <63070686+neildc14@users.noreply.github.com>
2023-04-16 19:13:04 +0800

    modified:PersonneTripTickets

--- client/src/components/ambulance-personnel/PersonnelTripTickets.jsx
+++ client/src/components/ambulance-personnel/PersonnelTripTickets.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import {
   Box,
   Divider,
@@ -10,25 +10,39 @@ import {
 } from "@chakra-ui/react";
 import TripTicketCard from "../global/TripTicketCard";
 import { UilHistory, UilFileInfoAlt } from "@iconscout/react-unicons";
-
 import TripTicketDetails from "../global/TripTicketDetails";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const ENDPOINT = import.meta.env.VITE_REACT_APP_ENDPOINT;
 
 const PersonnelTripTickets = () => {
   const [isLargerThan768] = useMediaQuery("(min-width: 768px)");
   const [show, setShow] = useState(false);
+  const [ticketDetails, setTicketDetails] = useState({});
+
+  const fetchTripTickets = useCallback(async () => {
+    const response = await axios.get(`${ENDPOINT}ticket`);
+    return response.data;
+  }, []);
+
+  const queryKey = "trip_ticket";
+  const { data, isLoading, isFetching, error } = useQuery(
+    [queryKey],
+    fetchTripTickets,
+    {
+      refetchOnWindowFocus: false,
+    }
+  );
+
+  const memoizedData = useMemo(() => {
+    return data;
+  }, [data]);
 
   const showTripTicketDetails = () => {
     setShow(true);
   };
 
-  let count = 10;
-
-  const listItems = [];
-  for (let i = 0; i < count; i++) {
-    listItems.push(
-      <TripTicketCard showTripTicketDetails={showTripTicketDetails} />
-    );
-  }
   return (
     <>
       <Box>
@@ -57,40 +71,66 @@ const PersonnelTripTickets = () => {
               </Box>
               <Box overflowY={{ md: "scroll" }} height={{ md: "80vh" }}>
                 <Flex flexDirection="column" gap={4} pe={{ md: 4 }} pt={4}>
-                  {listItems}
+                  {!error &&
+                    memoizedData?.map((trip_ticket) => (
+                      <TripTicketCard
+                        key={trip_ticket._id}
+                        trip_ticket={trip_ticket}
+                        showTripTicketDetails={showTripTicketDetails}
+                        ticketDetails={ticketDetails}
+                        setTicketDetails={setTicketDetails}
+                      />
+                    ))}
+                  {error && (
+                    <Card bgColor="orange.300">
+                      <CardBody
+                        display="inline-flex"
+                        alignItems="center"
+                        gap={2}
+                        color="white"
+                        fontWeight="semibold"
+                      >
+                        No tickets found.
+                      </CardBody>
+                    </Card>
+                  )}
                 </Flex>
               </Box>
             </Box>
           </GridItem>
 
-          <GridItem colSpan={2}>
-            <Box
-              as="aside"
-              height="100%"
-              px={4}
-              py={4}
-              bgColor="gray.50"
-              borderLeft="1px solid #D9D9D9"
-            >
-              <Box pb={4}>
-                <Flex justifyContent="space-between" alignItems="center">
-                  <Heading
-                    as="h2"
-                    display="inline-flex"
-                    gap={2}
-                    py={2}
-                    fontSize="lg"
-                    fontWeight="semibold"
-                    color="gray.700"
-                  >
-                    <UilFileInfoAlt color="#FF7A00" /> Trip Details
-                  </Heading>
-                </Flex>
-                <Divider />
-              </Box>
-              {isLargerThan768 && show && <TripTicketDetails />}
-            </Box>
-          </GridItem>
+          {!error && (
+            <GridItem colSpan={2}>
+              {isLargerThan768 && (
+                <Box
+                  as="aside"
+                  height="100%"
+                  px={4}
+                  py={4}
+                  bgColor="gray.50"
+                  borderLeft="1px solid #D9D9D9"
+                >
+                  <Box pb={4}>
+                    <Flex justifyContent="space-between" alignItems="center">
+                      <Heading
+                        as="h2"
+                        display="inline-flex"
+                        gap={2}
+                        py={2}
+                        fontSize="lg"
+                        fontWeight="semibold"
+                        color="gray.700"
+                      >
+                        <UilFileInfoAlt color="#FF7A00" /> Trip Details
+                      </Heading>
+                    </Flex>
+                    <Divider />
+                  </Box>
+                  {show && <TripTicketDetails ticketDetails={ticketDetails} />}
+                </Box>
+              )}
+            </GridItem>
+          )}
         </Grid>
       </Box>
     </>
