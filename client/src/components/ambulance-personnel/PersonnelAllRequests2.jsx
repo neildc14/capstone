1bec215656695176e79a44f1ee8667fefb037556
Neil Edward Dela Cruz <63070686+neildc14@users.noreply.github.com>
2023-04-18 22:59:17 +0800

    feature: personnel pending requests search:

--- client/src/components/ambulance-personnel/PersonnelAllRequests2.jsx
+++ client/src/components/ambulance-personnel/PersonnelAllRequests2.jsx
@@ -17,13 +17,14 @@ import { UilSearch, UilLayerGroup } from "@iconscout/react-unicons";
 import PaginatedItems from "../global/PaginatedItems";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-
 import PersonnelGenericRequestCard from "./PersonnelGenericRequestCard";
+import SearchBar from "../global/SearchBar";
 
 const ENDPOINT = import.meta.env.VITE_REACT_APP_ENDPOINT;
 
 const PersonnelRequests2 = () => {
   const [selectedTab, setSelectedTab] = useState(0);
+  const [search, setSearch] = useState([]);
 
   const fetchAllRequests = useCallback(async () => {
     const response = await axios.get(`${ENDPOINT}request`);
@@ -40,22 +41,10 @@ const PersonnelRequests2 = () => {
   }, [data]);
 
   let pendingRequests = [];
-  let approvedRequest = [];
-  let fulfilledRequest = [];
-  let rejectedRequests = [];
 
   const filterRequests = () => {
     if (Array.isArray(memoizedData)) {
       pendingRequests = memoizedData?.filter((req) => req.status === "pending");
-      approvedRequest = memoizedData?.filter(
-        (req) => req.status === "approved"
-      );
-      fulfilledRequest = memoizedData?.filter(
-        (req) => req.status === "fulfilled"
-      );
-      rejectedRequests = memoizedData?.filter(
-        (req) => req.status === "rejected"
-      );
     }
   };
   filterRequests();
@@ -98,73 +87,93 @@ const PersonnelRequests2 = () => {
                 >
                   <UilLayerGroup color="#FF7A00" /> All Pending Requests
                 </Heading>
-                <Box display="inline-flex" flex="1" gap={2}>
-                  <Input
-                    type="search"
-                    size="sm"
-                    flex="1"
-                    placeholder="Search a request"
-                  />
-                  <Button type="submit" display="inline-flex" gap={2} size="sm">
-                    <UilSearch size="16px" /> Search
-                  </Button>
-                </Box>
+                <SearchBar
+                  memoizedData={pendingRequests}
+                  setSearch={setSearch}
+                  placeholder="Search a request"
+                  noResultMessage="No request found."
+                />
               </Flex>
               <Divider />
             </Box>
           </Box>
-          <Tabs
-            variant="enclosed"
-            isLazy
-            selectedindex={selectedTab}
-            onChange={(index) => setSelectedTab(index)}
-          >
-            <TabList overflowX="scroll" overflowY="hidden">
-              {tabs?.map((tab, index) => (
-                <Tab
-                  key={tab.label}
-                  fontSize={{ base: "xs", md: "md" }}
-                  _selected={{ color: "orange.500", fontWeight: "semibold" }}
-                >
-                  {tab.label}{" "}
-                  <Text as="span" opacity={selectedTab === index ? 1 : 0}>
-                    {`(${tab.counter})`}
-                  </Text>
-                </Tab>
-              ))}
-            </TabList>
-
-            <TabPanels bgColor="custom.secondary" mt={4} py={2}>
-              {!error &&
-                tabs?.map((tab) => (
-                  <TabPanel key={tab.label + "panel"}>
-                    <Flex flexDirection="column" gap={4}>
-                      <PaginatedItems itemsPerPage={4} items={tab.items}>
-                        {(currentItems) => (
-                          <Flex flexDirection="column" gap={2}>
-                            {currentItems !== undefined &&
-                              currentItems.map((item, i) => (
-                                <PersonnelGenericRequestCard
-                                  request_data={item}
-                                  key={item?._id}
-                                  borderRadius="sm"
-                                  name={`${item?.first_name} ${item?.last_name}`}
-                                  date_time={item?.createdAt}
-                                />
-                              ))}
-                            {tab?.counter === 0 && (
-                              <Text fontSize="md" color="orange.500">
-                                No requests found
-                              </Text>
-                            )}
-                          </Flex>
-                        )}
-                      </PaginatedItems>
-                    </Flex>
-                  </TabPanel>
+          {search?.length <= 0 && (
+            <Tabs
+              variant="enclosed"
+              isLazy
+              selectedindex={selectedTab}
+              onChange={(index) => setSelectedTab(index)}
+            >
+              <TabList overflowX="scroll" overflowY="hidden">
+                {tabs?.map((tab, index) => (
+                  <Tab
+                    key={tab.label}
+                    fontSize={{ base: "xs", md: "md" }}
+                    _selected={{ color: "orange.500", fontWeight: "semibold" }}
+                  >
+                    {tab.label}{" "}
+                    <Text as="span" opacity={selectedTab === index ? 1 : 0}>
+                      {`(${tab.counter})`}
+                    </Text>
+                  </Tab>
                 ))}
-            </TabPanels>
-          </Tabs>
+              </TabList>
+
+              <TabPanels bgColor="custom.secondary" mt={4} py={2}>
+                {!error &&
+                  tabs?.map((tab) => (
+                    <TabPanel key={tab.label + "panel"}>
+                      <Flex flexDirection="column" gap={4}>
+                        <PaginatedItems itemsPerPage={4} items={tab.items}>
+                          {(currentItems) => (
+                            <Flex flexDirection="column" gap={2}>
+                              {currentItems !== undefined &&
+                                currentItems.map((item, i) => (
+                                  <PersonnelGenericRequestCard
+                                    request_data={item}
+                                    key={item?._id}
+                                    borderRadius="sm"
+                                    name={`${item?.first_name} ${item?.last_name}`}
+                                    date_time={item?.createdAt}
+                                  />
+                                ))}
+                              {tab?.counter === 0 && (
+                                <Text fontSize="md" color="orange.500">
+                                  No requests found
+                                </Text>
+                              )}
+                            </Flex>
+                          )}
+                        </PaginatedItems>
+                      </Flex>
+                    </TabPanel>
+                  ))}
+              </TabPanels>
+            </Tabs>
+          )}
+
+          {search?.length > 0 && !error && (
+            <Box bgColor="custom.secondary" mt={4} py={8} px={4}>
+              <Flex flexDirection="column" gap={4}>
+                <PaginatedItems itemsPerPage={4} items={search}>
+                  {(currentItems) => (
+                    <Flex flexDirection="column" gap={2}>
+                      {currentItems !== undefined &&
+                        currentItems.map((item, i) => (
+                          <PersonnelGenericRequestCard
+                            request_data={item}
+                            key={item?._id}
+                            borderRadius="sm"
+                            name={`${item?.first_name} ${item?.last_name}`}
+                            date_time={item?.createdAt}
+                          />
+                        ))}
+                    </Flex>
+                  )}
+                </PaginatedItems>
+              </Flex>
+            </Box>
+          )}
         </Box>
       </Box>
     </>
