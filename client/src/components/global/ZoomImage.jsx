18d590d925bf00e2f178389ea98f8beb333f1b0c
Neil Edward Dela Cruz <63070686+neildc14@users.noreply.github.com>
2023-05-10 13:55:07 +0800

    modified Zoomimage

--- client/src/components/global/ZoomImage.jsx
+++ client/src/components/global/ZoomImage.jsx
@@ -11,21 +11,19 @@ import {
 } from "@chakra-ui/react";
 export default function ZoomImage({ isOpen, onClose, image }) {
   return (
-    <Modal onClose={onClose} size="4xl" isOpen={isOpen}>
+    <Modal onClose={onClose} size={{ base: "full", md: "4xl" }} isOpen={isOpen}>
       <ModalOverlay />
       <ModalContent>
         <Box zIndex={2000000} color="#FF7A00">
           <ModalCloseButton />
         </Box>
         <ModalBody p={3}>
-          <AspectRatio ratio={16 / 9} overflow="hidden">
-            <Image
-              src={image}
-              postion="center"
-              mx="auto"
-              width="100%"
-              height="100%"
-            />  
+          <AspectRatio
+            ratio={{ base: 1, md: 16 / 9 }}
+            overflow="hidden"
+            my={{ base: 10, md: 0 }}
+          >
+            <Image src={image} postion="center" width="100%" height="100%" />
           </AspectRatio>
         </ModalBody>
       </ModalContent>
